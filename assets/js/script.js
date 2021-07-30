6b0d3ad57a090495cf9dc4282494f29892bf30d6
mg747 <morganugiagbe77@gmail.com>
2021-07-30 17:20:38 +0000

    I added a JavaScript scroll function to the homepage to enable user to return to top of page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
-let largeFont = document.getElementById('large-font');
+//Get the button:
+mybutton = document.getElementById("myBtn");
 
-console.log('Text content of the large font is:', largeFont.textContent);
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
